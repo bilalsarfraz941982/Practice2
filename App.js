ecd4ace8f5d4f2c1352ff1fd5f8990eb10d086ba
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-10-16 10:23:13 +0100

    a

--- App.js
+++ App.js
@@ -7,7 +7,13 @@
 
         function MainFucntion($scope){
             $scope.name = "Bilal Sarfraz";
-            
+            $scope.myAge ="";
+
+
+            $scope.age = function(){
+                $scope.myAge = "i am 42 years old baby :)"
+                return $scope.myAge
+            }
         };
     }
 )();
